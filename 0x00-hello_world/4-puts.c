845b708a889e91a249c99dbfe4d34ad600f21992
juanctorresn <2387@holbertonschool.com>
2020-09-18 22:10:45 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
-/ * *
-* principal - pone mensaje
-
-* Usar función put, no usar printf
-* Devolución: Valor 0
-
-* /
-    int  main
- {
-put("\"Programar es como construir un rompecabezas multilingüe");
-return(0);
+#include <stdio.h>
+/**
+ * main - puts message
+ * Use function puts, dont use printf
+ * Return: Value 0
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
