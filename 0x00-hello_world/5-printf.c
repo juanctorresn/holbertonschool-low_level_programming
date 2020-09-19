d5612121285de41f8f75933e0a2ba14b26344ee6
juanctorresn <2387@holbertonschool.com>
2020-09-19 03:46:05 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - using printf
+ * Use the function printf, compile without warning
+ * when using the -Wall gcc option
+ * Return: value 0
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
