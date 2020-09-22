fe08885c3e760188249f67aeb062b2191082d5a8
juanctorresn <2387@holbertonschool.com>
2020-09-22 23:04:48 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+# incluye  < stdio.h >
+
+/* 
+ * this main prints Holberton, followed by a new line.
+ * return 0
+ */
+int main (void)
+{ 
+  putchar (72);
+  putchar (111);
+  putchar( 108);
+  putchar (98);
+  putchar (101);
+  putchar (114);
+  putchar (116);
+  putchar (111);
+  putchar (110);
+  return (0)
+    }
