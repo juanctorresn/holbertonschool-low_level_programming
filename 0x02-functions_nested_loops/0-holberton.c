b38e4060d4bc1f9954efd4fd915471b68fb7f413
juanctorresn <2387@holbertonschool.com>
2020-09-23 21:31:29 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "holberton.h"
 /* 
  * this main prints Holberton, followed by a new line.
@@ -6,15 +5,15 @@
  */
 int main (void)
 { 
- _putchar (72);
- _putchar (111);
- _putchar (108);
- _putchar (98);
- _putchar (101);
- _putchar (114);
- _putchar (116);
- _putchar (111);
- _putchar (110);
+  _putchar 72;
+  _putchar 111;
+  _putchar 108;
+  _putchar 98;
+  _putchar 101;
+  _putchar 114;
+  _putchar 116;
+  _putchar 111;
+  _putchar 110;
 
  return 0;
     }
