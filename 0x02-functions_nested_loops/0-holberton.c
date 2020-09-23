b3bc027a75156a329e20a95288958bfd31246e33
juanctorresn <2387@holbertonschool.com>
2020-09-23 03:35:15 +0000

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,14 +6,14 @@
  */
 int main (void)
 { 
- _ putchar (72);
- _ putchar (111);
- _ putchar( 108);
- _ putchar (98);
- _ putchar (101);
- _ putchar (114);
- _ putchar (116);
- _ putchar (111);
- _ putchar (110);
+ _putchar (72);
+ _putchar (111);
+ _putchar (108);
+ _putchar (98);
+ _putchar (101);
+ _putchar (114);
+ _putchar (116);
+ _putchar (111);
+ _putchar (110);
   return (0)
     }
