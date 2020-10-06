b7e83ea7453d552769698f59e96df6ce346270a7
juanctorresn <2387@holbertonschool.com>
2020-10-06 01:07:15 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+#include <stdio.h>
+/*
+
+ */
+
+
+void simple_print_buffer(char *buffer, unsigned int size)
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+learning Even more pointers, arrays and strings and the use of memset 
