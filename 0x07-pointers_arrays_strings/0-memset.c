ecb8eda4a6d2e2cf2e38168496e70030bd341cb9
juanctorresn <2387@holbertonschool.com>
2020-10-06 03:47:49 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /*
-
+ *using _memset
+ *writing a function that fills memory with a constant byte
  */
 
+#define b
+void char *_memset(char *s,char b, unsigned int n)
+{ 
+	unsigned int a = 0
+		b = a
 
-void simple_print_buffer(char *buffer, unsigned int size)
+}
